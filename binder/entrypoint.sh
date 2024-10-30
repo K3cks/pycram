f5fd33abfb76c40d24e7ba18470bef2a0b25a5ba
Vanessa Hassouna <33067562+sunava@users.noreply.github.com>
2024-10-30 20:21:02 +0100

    Update entrypoint.sh

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-sudo systemctl start mongod
+
 source ${PYCRAM_WS}/devel/setup.bash &
+
 roscore &
 roslaunch pycram ik_and_description.launch robot:='pr2'&
 roslaunch suturo_knowledge suturo_knowledge.launch &
 roslaunch knowrob_designator knowrob_designator_service.launch &
+sudo systemctl start mongod &
+
 roslaunch rvizweb rvizweb.launch config_file:=${PYCRAM_WS}/src/pycram/binder/rviz_configs/pr2_config.json &
 cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json 
-
 
 xvfb-run exec "$@"
