fe9f0eae237e55a8bf44bf4d1f832e7517839d0a
pkehr <philk99@web.de>
2024-11-25 12:16:04 +0100

    [demo] fixed iteration of loops, added function wait_for_all_processes, adjusted robots pose, reduced imports

--- demos/pycram_multirobot_demo/scenarios/party_apartment.py
+++ demos/pycram_multirobot_demo/scenarios/party_apartment.py
@@ -1,13 +1,6 @@
-from typing import List
-
-import rospy
-
-from demos.pycram_multirobot_demo.demo_manager import robot_one
-from demos.pycram_multirobot_demo.setup.actions import actions
 from demos.utils.enums import ROBOTS, ENVIRONMENTS
-from demos.utils.launcher import launch_robot, launch_all_robots
+from demos.utils.launcher import launch_all_robots
 from demos.utils.object_spawner import create_robot, set_environment
-from pycram.datastructures.pose import Pose
 from pycram.designators.action_designator import *
 from pycram.multirobot.multi_threaded_robots import MultiThreadedRobot
 from pycram.process_module import simulated_robot
@@ -22,6 +15,8 @@ def robot_one_actions(robot, iterations=1):
             rospy.sleep(delay)
             PartyAction(arms=[Arms.BOTH], used_robot=robot).resolve().perform()
             rospy.sleep(delay)
+            i = i + 1
+
 
 def robot_two_actions(robot, iterations=1):
     i = 0
@@ -32,6 +27,8 @@ def robot_two_actions(robot, iterations=1):
             rospy.sleep(delay)
             PartyAction(arms=[Arms.BOTH], used_robot=robot).resolve().perform()
             rospy.sleep(delay)
+            i = i + 1
+
 
 def robot_three_actions(robot, iterations=1):
     i = 0
@@ -42,6 +39,7 @@ def robot_three_actions(robot, iterations=1):
             rospy.sleep(delay)
             PartyAction(arms=[Arms.BOTH], used_robot=robot).resolve().perform()
             rospy.sleep(delay)
+            i = i + 1
 
 
 def robot_four_actions(robot, iterations=1):
@@ -53,6 +51,8 @@ def robot_four_actions(robot, iterations=1):
             rospy.sleep(delay)
             PartyAction(arms=[Arms.BOTH], used_robot=robot).resolve().perform()
             rospy.sleep(delay)
+            i = i + 1
+
 
 def party_apartment(robots: List[ROBOTS], launch_robots=True):
     if launch_robots:
@@ -63,12 +63,12 @@ def party_apartment(robots: List[ROBOTS], launch_robots=True):
     robot_three = robots[2]
     robot_four = robots[3]
 
-    pose_pr2 = Pose([0, 1, 0])
-    pose_tiago = Pose([0, 3, 0])
-    pose_justin = Pose([3, 3, 0])
-    pose_icub = Pose([3, 0, 0])
+    pose_pr2 = Pose([1.5, 2.5, 0])
+    pose_tiago = Pose([4.5, 5.3, 0], [0, 0, -0.2, 0.3])
+    pose_justin = Pose([5, 2, 0], [0, 0, 0.2, 0.3])
+    pose_icub = Pose([4, 3.8, 0], [0, 0, 0, 1])
 
-    # current_environment = set_environment(ENVIRONMENTS.APARTMENT_SMALL)
+    current_environment = set_environment(ENVIRONMENTS.APARTMENT_SMALL)
 
     first_robot = create_robot(robot_one, pose=pose_pr2)
     second_robot = create_robot(robot_two, pose=pose_tiago)
@@ -81,7 +81,4 @@ def party_apartment(robots: List[ROBOTS], launch_robots=True):
     process2 = mtr.start_process(robot_two_actions, (second_robot, 5))
     process3 = mtr.start_process(robot_three_actions, (third_robot, 7))
     process4 = mtr.start_process(robot_four_actions, (fourth_robot, 9))
-    mtr.end(process)
-    mtr.end(process2)
-    mtr.end(process3)
-    mtr.end(process4)
+    mtr.wait_for_all_processes()
